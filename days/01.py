36a154870c9f3fb2ea8b5c661f1639e2d98ee5d4
Malaber <git@schaedler.rocks>
2021-12-07 17:37:15 +0100

    Add solution for 01 2

--- days/01.py
+++ days/01.py
@@ -9,14 +9,35 @@ lines = Input.get_lines(os.path.basename(__file__))
 
 depths = [int(depth) for depth in lines]
 
-inc = 0
+def count_increases(depths):
+    inc = 0
+    for i in range(len(depths)):
+        this = depths[i]
+        last = depths[i - 1] if i > 0 else None
 
+        if last:
+            if this > last:
+                inc += 1
+    return inc
+
+
+print("Part 1")
+print(count_increases(depths))
+
+
+print("Part 2")
+
+windows = []
 for i in range(len(depths)):
-    this = depths[i]
-    last = depths[i-1] if i > 0 else None
+    # print(i)
+    a, b, c = 0, 0, 0
+    try:
+        a = depths[i]
+        b = depths[i+1]
+        c = depths[i+2]
+    except IndexError as e:
+        continue
 
-    if last:
-        if this > last:
-            inc += 1
+    windows.append(a+b+c)
 
-print(inc)
+print(count_increases(windows))
