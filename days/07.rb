5ea5ec0dfd4ebe564d6397d1f88bb6581432f27d
Malaber <git@schaedler.rocks>
2021-12-07 14:46:51 +0100

    Add simple implementation of 07 2

--- days/07.rb
+++ days/07.rb
@@ -5,6 +5,7 @@ positions = lines.first.split(',')
 
 positions = positions.map(&:to_i)
 
+puts "PART 1"
 minfuel = nil
 best_pos = -1
 
@@ -12,6 +13,29 @@ best_pos = -1
   fuel = 0
   positions.each do |p|
     fuel += (pos - p).abs
+  end
+  if minfuel.nil? || minfuel > fuel
+    minfuel = fuel
+    best_pos = pos
+  end
+end
+
+puts "Fuel: #{minfuel}, position: #{best_pos}"
+
+puts
+puts "PART 2"
+
+minfuel = nil
+best_pos = -1
+
+(positions.min..positions.max).each do |pos|
+  fuel = 0
+  positions.each do |p|
+    move = (pos - p).abs
+    if move > 0
+      move_fuel = (1..move).inject(:+)
+      fuel += move_fuel
+    end
   end
   if minfuel.nil? || minfuel > fuel
     minfuel = fuel
