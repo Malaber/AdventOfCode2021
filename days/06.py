1b79510409c69c34aefd8b9e3f1fa885e53427ad
Malaber <git@schaedler.rocks>
2021-12-07 16:58:42 +0100

    Optimize day 06 to get answer for part 2

--- days/06.py
+++ days/06.py
@@ -10,29 +10,36 @@ lines = Input.get_lines(os.path.basename(__file__))
 
 fishes = [int(fish) for fish in lines[0].split(",")]
 
+fishes_compact = {}
+possible_values = range(0, 9)
+for i in possible_values:
+    fishes_compact[i]=fishes.count(i)
+
+print(fishes_compact)
+
 final_day = 256
 day = 0
 
 
-def print_fishes(days, fishes):
-    print(f"Day {day}: {len(fishes)}")
+def print_fishes(day, fishes):
+    print(f"Day {day}: {sum(fishes.values())}")
 
 
 while day != final_day:
-    new_fishes = 0
-    for id, fish in enumerate(fishes):
-        if fish == 0:
-            fishes[id] = 6
-            new_fishes += 1
-        else:
-            fishes[id] -= 1
-    for _ in itertools.repeat(None, new_fishes):
-        fishes.append(8)
+    new_fishes = {}
+    for k,v in fishes_compact.items():
+        new_fishes[k-1] = v
+    new_fishes[8] = new_fishes[-1]
+    new_fishes[6] += new_fishes[-1]
+    del new_fishes[-1]
+
+    fishes_compact = new_fishes
+
     day += 1
-    print(day)
+    #print(day)
     if day == 80:
         print("Part 1")
-        print_fishes(day, fishes)
+        print_fishes(day, fishes_compact)
 
 print("Part 2")
-print_fishes(final_day, fishes)
+print_fishes(final_day, fishes_compact)
