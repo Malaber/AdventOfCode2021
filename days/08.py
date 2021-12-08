649ce9adfcd7bc0d92ee52b62a3381f5bf4425a2
Malaber <git@schaedler.rocks>
2021-12-08 11:35:34 +0100

    Add python files with example for day 08

---
+++ days/08.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))  # I am not proud of this
+
+from helpers.input import Input
+
+lines = Input.get_lines(os.path.basename(__file__))
+
