5bb7091e492b28f8b333e9ab95a84dfcbec32272
Malaber <git@schaedler.rocks>
2021-12-08 13:43:19 +0100

    Finished decoding all digits

--- days/08.py
+++ days/08.py
@@ -45,7 +45,7 @@ for line in lines:
         sorted_digit = "".join(sorted(digit))
         input_digits_length[len(digit)].append(sorted_digit)
 
-    while len(decoding.keys()) < 10:
+    while len(decoding_per_digit.keys()) < 10:
         one = input_digits_length[2][0]
         decoding_per_digit[1] = one
         four = input_digits_length[4][0]
@@ -75,20 +75,32 @@ for line in lines:
                 decoding_per_digit[2] = digit
                 break
 
-        for digit in input_digits_length[5]:
-            digit_without_4 = remove_chars(four, digit)
-            digit_without_known = remove_chars("".join(decoding.keys()), digit_without_4)
-            if len(digit_without_known) == 1:
-                # the digit is five
-                decoding[digit_without_known] = "e"
-                decoding_per_digit[5] = digit
-                break
-
         four_without_one = remove_chars(one, four)
         four_without_two = remove_chars(decoding_per_digit[2], four_without_one)
         decoding[four_without_two] = "b"
 
-        print()
+        for digit in input_digits_length[5]:
+            # looking for 5
+            digit_without_two = remove_chars(decoding_per_digit[2], digit)
+            digit_without_one = remove_chars(one, digit_without_two)
+            if len(digit_without_known) == 1:
+                # the digit is five
+                decoding_per_digit[5] = digit
+                break
+
+        for digit in input_digits_length[5]:
+            if digit not in [decoding_per_digit[2], decoding_per_digit[5]]:
+                # the digit is 3
+                decoding_per_digit[3] = digit
+
+        for digit in input_digits_length[6]:
+            digit_without_5 = remove_chars(decoding_per_digit[5], digit)
+            if len(digit_without_5) == 1 and digit != decoding_per_digit[9]:
+                # digit is 6
+                decoding_per_digit[6] = digit
+            else:
+                # digit is 0
+                decoding_per_digit[0] = digit
 
 
 print("Part 1")
