779c441105fb40b738fe0ccc800280588df266b9
Malaber <git@schaedler.rocks>
2021-12-08 11:46:22 +0100

    Add solution for day 08 1

--- days/08.py
+++ days/08.py
@@ -7,3 +7,14 @@ from helpers.input import Input
 
 lines = Input.get_lines(os.path.basename(__file__))
 
+print("Part 1")
+occurences = 0
+for line in lines:
+    input, output = line.split(" | ")
+    output_digits = output.strip().split(" ")
+    for digit in output_digits:
+        unique_digit_amounts = [2, 4, 3, 7]
+        if len(digit) in unique_digit_amounts:
+            occurences += 1
+
+print(occurences)
