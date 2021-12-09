9f916bcd2086a2effa60c047bb2d0b07f98387be
Malaber <git@schaedler.rocks>
2021-12-09 17:48:49 +0100

    Add solution to 09 a that works for example

--- days/09.py
+++ days/09.py
@@ -19,53 +19,58 @@ lines = Input.get_lines(os.path.basename(__file__))
 lines = PositiveList([PositiveList([int(height) for height in list(line.strip())]) for line in lines])
 
 
-def get_lowest_point(x, y):
+def get_lowest_point(x, y, lines):
     point = lines[x][y]
 
     up_x = x-1
     try:
         up = lines[up_x][y]
-    except IndexError:
+    except (TypeError, IndexError):
         up = None
 
     down_x = x+1
     try:
         down = lines[down_x][y]
-    except IndexError:
+    except (TypeError, IndexError):
         down = None
 
-    left_y = x-1
+    left_y = y-1
     try:
         left = lines[x][left_y]
-    except IndexError:
+    except (TypeError, IndexError):
         left = None
 
-    right_y = x+1
+    right_y = y+1
     try:
         right = lines[x][right_y]
-    except IndexError:
+    except (TypeError, IndexError):
         right = None
 
-    if up and up < point:
-        return get_lowest_point(up_x, y)
+    if up is not None and up < point:
+        return get_lowest_point(up_x, y, lines)
 
-    if down and down < point:
-        return get_lowest_point(down_x, y)
+    if down is not None and down < point:
+        return get_lowest_point(down_x, y, lines)
 
-    if left and left < point:
-        return get_lowest_point(x, left_y)
+    if left is not None and left < point:
+        return get_lowest_point(x, left_y, lines)
 
-    if right and right < point:
-        return get_lowest_point(x, right_y)
+    if right is not None and right < point:
+        return get_lowest_point(x, right_y, lines)
 
-    return x, y
+    return x, y, point
 
 
 low_points = set()
 
 for x, line in enumerate(lines):
     for y, height in enumerate(line):
-        low_points.add(get_lowest_point(x, y))
+        low_points.add(get_lowest_point(x, y, lines))
 
-print(low_points)
-pass
+sum_of_risk = 0
+for point in low_points:
+    risk_level = 1 + point[2]
+    sum_of_risk += risk_level
+
+print("Part 1:")
+print(sum_of_risk)
