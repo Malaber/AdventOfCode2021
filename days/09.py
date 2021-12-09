411d8c5a81e8227ddb5efd26c78191ba0e306fe2
Malaber <git@schaedler.rocks>
2021-12-09 18:32:38 +0100

    I give up now

--- days/09.py
+++ days/09.py
@@ -18,9 +18,18 @@ lines = Input.get_lines(os.path.basename(__file__))
 
 lines = PositiveList([PositiveList([int(height) for height in list(line.strip())]) for line in lines])
 
+calls = 0
 
-def get_lowest_point(x, y, lines):
+
+def get_lowest_point(x, y, lines, init=False):
+    global calls
+    if init:
+        calls += 1
+
     point = lines[x][y]
+
+    # if point == 0:
+    #     return x, y, point
 
     up_x = x-1
     try:
@@ -46,31 +55,45 @@ def get_lowest_point(x, y, lines):
     except (TypeError, IndexError):
         right = None
 
+    nearest_lowest = set()
+
     if up is not None and up < point:
-        return get_lowest_point(up_x, y, lines)
+        nearest_lowest.union(get_lowest_point(up_x, y, lines))
 
     if down is not None and down < point:
-        return get_lowest_point(down_x, y, lines)
+        nearest_lowest.union(get_lowest_point(down_x, y, lines))
 
     if left is not None and left < point:
-        return get_lowest_point(x, left_y, lines)
+        nearest_lowest.union(get_lowest_point(x, left_y, lines))
 
     if right is not None and right < point:
-        return get_lowest_point(x, right_y, lines)
+        nearest_lowest.union(get_lowest_point(x, right_y, lines))
 
-    return x, y, point
+    if right == left and left == up and up == down and down == point:
+        return set()
+
+    if len(nearest_lowest) == 0:
+        nl_set = set()
+        nl_set.add((x, y, point))
+        return nl_set
+    else:
+        return nearest_lowest
 
 
 low_points = set()
 
 for x, line in enumerate(lines):
     for y, height in enumerate(line):
-        low_points.add(get_lowest_point(x, y, lines))
+        for point in get_lowest_point(x, y, lines, True):
+            if type(point) == int:
+                print(x, y, point)
+            low_points.add(point)
 
 sum_of_risk = 0
-for point in low_points:
-    risk_level = 1 + point[2]
+for low_point in low_points:
+    risk_level = 1 + low_point[2]
     sum_of_risk += risk_level
 
 print("Part 1:")
+print(calls)
 print(sum_of_risk)
